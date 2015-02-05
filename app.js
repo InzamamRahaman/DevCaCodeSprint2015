6caace8ed29a7e23f31aa6001f632f897981f762
Inzamam <izzyrahaman@gmail.com>
2015-02-05 14:10:16 -0400

    Added mongodb models

--- app.js
+++ app.js
@@ -1,60 +1,88 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var express = require('express');
+//var path = require('path');
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var bodyParser = require('body-parser');
+//
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
+//
+//var app = express();
+//
+//// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+//
+//// uncomment after placing your favicon in /public
+////app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(logger('dev'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+//
+//app.use('/', routes);
+//app.use('/users', users);
+//
+//// catch 404 and forward to error handler
+//app.use(function(req, res, next) {
+//    var err = new Error('Not Found');
+//    err.status = 404;
+//    next(err);
+//});
+//
+//// error handlers
+//
+//// development error handler
+//// will print stacktrace
+//if (app.get('env') === 'development') {
+//    app.use(function(err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err
+//        });
+//    });
+//}
+//
+//// production error handler
+//// no stacktraces leaked to user
+//app.use(function(err, req, res, next) {
+//    res.status(err.status || 500);
+//    res.render('error', {
+//        message: err.message,
+//        error: {}
+//    });
+//});
+//
+//
+//module.exports = app;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+var express = require("express");
+var path = require("path");
+var config = require("./lib/config");
+var mongoose = require("mongoose");
+var mongooseAPI = require("mongoose-api");
+var app = config.init(express());
+var http = require("http").Server(app);
+mongooseAPI.serveModels(app);
+var db = mongoose.connection;
+var static_loc = path.join(path.join(__dirname, 'app'), 'views');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static(static_loc));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var subs = require("./lib/subscription-manager")(http, db);
 
-app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
 
-// error handlers
+db.on('error', console.error.bind(console, "console.log"));
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+db.once('open', function() {
+
+    http.createServer(app).listen(config.port, function() {
+       console.log('Express is running :D');
     });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
 });
-
-
-module.exports = app;
