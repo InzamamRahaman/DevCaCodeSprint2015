7953592e272b5d90504dcc1d1c96dd8f0ffb3d05
Inzamam <izzyrahaman@gmail.com>
2015-02-05 15:00:57 -0400

    Added streaming

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ mongoose.connection.once('open', function() {
 
     http.listen(config.port, function(){
         console.log("Listening on http://127.0.0.1:"+config.port);
+        subs.start_streaming();
     });
 
 });
