bc8cf7f9ff38461537c7133eba0dcc944c1dc096
jherez taylor <jherez.taylor@gmail.com>
2015-02-06 04:57:49 -0400

    heat map is functional, but needs more data

--- app/js/site.js
+++ app/js/site.js
@@ -13,7 +13,7 @@ function initialize() {
   var mapOptions = {
     zoom: 13,
     center: new google.maps.LatLng(10.667672, -61.536567),
-    mapTypeId: google.maps.MapTypeId.SATELLITE
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   map = new google.maps.Map(document.getElementById('map-canvas'),
